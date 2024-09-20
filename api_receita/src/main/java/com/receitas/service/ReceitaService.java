7f56812f7b17b0bbf9ecdb25474c30e974c7e483
Matheus <mathgabriel@outlook.com.br>
2024-09-19 21:52:55 -0300

    Commit

--- api_receita/src/main/java/com/receitas/service/ReceitaService.java
+++ api_receita/src/main/java/com/receitas/service/ReceitaService.java
@@ -16,9 +16,32 @@ public class ReceitaService {
         listaDeReceitas.add(receitaFake);
     }
 
-    public ReceitaService(){
+    public ReceitaService() {
         receitaFake();
     }
 
-    
+    public List<Receita> getAll() {
+        return listaDeReceitas;
+    }
+
+    public Receita getById(long id) {
+        Receita _receita = new Receita(id);
+        return listaDeReceitas.stream()
+                            .filter(r ->.equals(_receita))
+                            .findFirst().orElse(other:null);
+
+    }
+
+    public List<Receita> getByIngredientes(String ingredientes) {
+        listaDeReceitas.stream()
+                .filter(r -> r.getIngredientes().stream().filter(i -> i.equals(ingredientes))).toList();
+    }
+
+    public boolean delete(Long id){
+        Receita receita = this.getById(id);
+        if (receita != ) {
+            
+        }
+    }
+
 }
